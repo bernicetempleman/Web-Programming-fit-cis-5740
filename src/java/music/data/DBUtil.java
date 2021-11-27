9b9ad213246d8053844821bbcdae21fab1744d30
Bernice Templeman <bernice.templeman@gmail.com>
2021-11-27 18:14:30 -0500

    Add files via upload

---
+++ src/java/music/data/DBUtil.java
@@ -0,0 +1,13 @@
+package music.data;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class DBUtil {
+    private static final EntityManagerFactory emf =
+            Persistence.createEntityManagerFactory("userAdminPU");
+    
+    public static EntityManagerFactory getEmFactory() {
+        return emf;
+    }
+}
